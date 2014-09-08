a05391889f5661e2f549ccde98dfcbd131a0641c
PetaroMitaro <mitranopeter@gmail.com>
2014-09-07 23:24:52 -0400

    removed unused HomePage and implemented Advisor Info

--- src/edu/wpi/tmathmeyer/mybannerwebwpi/page/AdvisorInfo.java
+++ src/edu/wpi/tmathmeyer/mybannerwebwpi/page/AdvisorInfo.java
@@ -3,6 +3,7 @@ package edu.wpi.tmathmeyer.mybannerwebwpi.page;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import android.util.Log;
@@ -25,16 +26,23 @@ public class AdvisorInfo implements Page {
 	}
 
 	private void parse() {
-		try {
-			Document doc = Jsoup.parse(html, "https://bannerweb.wpi.edu/pls/prod/");
-			Element body = doc.body();
-			Elements div = body.getElementsByClass("https://bannerweb.wpi.edu/pls/prod/");
-			for (Element e : div) {
-			}
-			Log.e("body", body.toString());
-		} catch (Exception e) {
-			for (StackTraceElement k : e.getStackTrace()) {
-				Log.d("BB+", "String Error!: " + k.toString());
+		Document doc = Jsoup.parse(html, "https://bannerweb.wpi.edu/pls/prod/");
+		Element body = doc.body();
+		Element pagebodydiv = body.getElementsByClass("pagebodydiv").first();
+		Elements bolds = pagebodydiv.getElementsByTag("b");
+		for (Element bold : bolds) {
+			Node node = bold.nextSibling();
+			String key = bold.text();
+			String sibling = bold.nextElementSibling().text();
+			String val = node.toString();
+			if (key.contains("Primary Advisor")) {
+				Log.e("keyval", key + " , " + val);
+			} else if (key.contains("Email")) {
+				Log.e("keyval", key + " , " + sibling);
+			} else if (key.contains("Advisor Department")) {
+				Log.e("keyval", key + " , " + val);
+			} else if (key.contains("Office Location")) {
+				Log.e("keyval", key + " , " + val);
 			}
 		}
 	}
