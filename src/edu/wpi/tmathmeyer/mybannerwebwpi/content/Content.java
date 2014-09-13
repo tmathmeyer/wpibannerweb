033f9c83f8b4383ecac9c812b1e748919fe53b1e
Cuong ND <nguyentricuong1995@gmail.com>
2014-09-13 01:04:31 -0400

    moved the items, map item_map and static block back to Content

--- src/edu/wpi/tmathmeyer/mybannerwebwpi/content/Content.java
+++ src/edu/wpi/tmathmeyer/mybannerwebwpi/content/Content.java
@@ -1,6 +1,17 @@
 package edu.wpi.tmathmeyer.mybannerwebwpi.content;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wpi.tmathmeyer.mybannerwebwpi.R;
 import edu.wpi.tmathmeyer.mybannerwebwpi.WebReader;
+import edu.wpi.tmathmeyer.mybannerwebwpi.page.AdvisorInfo;
+import edu.wpi.tmathmeyer.mybannerwebwpi.page.CalendarSchedule;
+import edu.wpi.tmathmeyer.mybannerwebwpi.page.DetailSchedule;
+import edu.wpi.tmathmeyer.mybannerwebwpi.page.MailBox;
+import edu.wpi.tmathmeyer.mybannerwebwpi.page.MealPlan;
 import edu.wpi.tmathmeyer.mybannerwebwpi.page.Page;
 
 /**
@@ -11,14 +22,37 @@ import edu.wpi.tmathmeyer.mybannerwebwpi.page.Page;
  */
 public class Content implements Runnable {
 
+	public static List<Page> items = new ArrayList<Page>();
+	public static Map<String, Page> item_map = new HashMap<String, Page>();
+
+	static {
+		addPage(new MailBox("Mail Box Information", "hwwkboxs.P_ViewBoxs",
+				R.layout.mailbox));
+		addPage(new MealPlan("Meal Plan Balances", "hwwkcbrd.P_Display",
+				R.layout.meal_plan));
+		// addPage(new CalendarSchedule("Calendar Schedule",
+		// "bwskfshd.P_CrseSchd?start_date_in=[DATE]",
+		// R.layout.calendar_schedule));
+		// addPage(new DetailSchedule("Detail Schedule",
+		// "bwskfshd.P_CrseSchdDetl", R.layout.detail_schedule));
+		// addPage(new AdvisorInfo("Acedemic Advisor Information",
+		// "hwwksadv.P_Summary", R.layout.advisor_info));
+	}
+
+	private static void addPage(Page page) {
+		items.add(page);
+		item_map.put(page.url, page);
+	}
+
 	public static void loadResources() {
-		for (Page i : Page.items) {
+		for (Page i : items) {
 			loadResource(i);
 		}
 	}
 
 	public static void loadResource(Page page) {
-		String pageHtml = WebReader.getInstance("", "").sendGetRequest("https://bannerweb.wpi.edu/pls/prod/" + page.url);
+		String pageHtml = WebReader.getInstance("", "").sendGetRequest(
+				"https://bannerweb.wpi.edu/pls/prod/" + page.url);
 		page.fillContent(pageHtml);
 	}
 
--- src/edu/wpi/tmathmeyer/mybannerwebwpi/page/Page.java
+++ src/edu/wpi/tmathmeyer/mybannerwebwpi/page/Page.java
@@ -1,32 +1,7 @@
 package edu.wpi.tmathmeyer.mybannerwebwpi.page;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import edu.wpi.tmathmeyer.mybannerwebwpi.R;
-
 public abstract class Page {
 
-
-	public static List<Page> items = new ArrayList<Page>();
-	public static Map<String,Page> item_map = new HashMap<String,Page>();
-	
-	static {
-		addPage(new MailBox("Mail Box Information", "hwwkboxs.P_ViewBoxs", R.layout.mailbox));
-		addPage(new MealPlan("Meal Plan Balances", "hwwkcbrd.P_Display", R.layout.meal_plan));
-		addPage(new CalendarSchedule("Calendar Schedule", "bwskfshd.P_CrseSchd?start_date_in=[DATE]", R.layout.calendar_schedule));
-		addPage(new DetailSchedule("Detail Schedule", "bwskfshd.P_CrseSchdDetl", R.layout.detail_schedule));
-		addPage(new AdvisorInfo("Acedemic Advisor Information", "hwwksadv.P_Summary", R.layout.advisor_info));
-	}
-	
-	private static void addPage(Page page) {
-		items.add(page);
-		item_map.put(page.url, page);
-	}
-
-	
 	public String url;
 	public String title;
 	public int layoutId;
