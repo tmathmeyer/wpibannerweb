f7df52984c9984ff97c2cf8971efbd0f50640ccb
PetaroMitaro <mitranopeter@gmail.com>
2014-09-13 01:01:50 -0400

    moved page list and map to Page class

--- src/edu/wpi/tmathmeyer/mybannerwebwpi/content/Content.java
+++ src/edu/wpi/tmathmeyer/mybannerwebwpi/content/Content.java
@@ -1,20 +1,6 @@
 package edu.wpi.tmathmeyer.mybannerwebwpi.content;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import edu.wpi.tmathmeyer.mybannerwebwpi.R;
 import edu.wpi.tmathmeyer.mybannerwebwpi.WebReader;
-import edu.wpi.tmathmeyer.mybannerwebwpi.page.AdvisorInfo;
-import edu.wpi.tmathmeyer.mybannerwebwpi.page.CalendarSchedule;
-import edu.wpi.tmathmeyer.mybannerwebwpi.page.DetailSchedule;
-import edu.wpi.tmathmeyer.mybannerwebwpi.page.MailBox;
-import edu.wpi.tmathmeyer.mybannerwebwpi.page.MealPlan;
 import edu.wpi.tmathmeyer.mybannerwebwpi.page.Page;
 
 /**
@@ -25,39 +11,10 @@ import edu.wpi.tmathmeyer.mybannerwebwpi.page.Page;
  */
 public class Content implements Runnable {
 
-	/**
-	 * An array of items.
-	 */
-	public static List<Page> items = new ArrayList<Page>();
-
-	/**
-	 * A map of items, by ID.
-	 */
-	public static Map<String, Page> item_map = new HashMap<String, Page>();
-
-	/**
-	 * Current list of agreed-upon items -mailbox (DONE) -meal plan (DONE)
-	 * -calendar schedule ( ) -detail schedule ( ) -advisor information ( )
-	 */
-
-	static {
-		addPage(new MailBox("Mail Box Information", "hwwkboxs.P_ViewBoxs", R.layout.mailbox));
-		addPage(new MealPlan("Meal Plan Balances", "hwwkcbrd.P_Display", R.layout.meal_plan));
-		addPage(new CalendarSchedule("Calendar Schedule", "bwskfshd.P_CrseSchd?start_date_in=[DATE]", R.layout.calendar_schedule));
-		addPage(new DetailSchedule("Detail Schedule", "bwskfshd.P_CrseSchdDetl", R.layout.detail_schedule));
-		addPage(new AdvisorInfo("Acedemic Advisor Information", "hwwksadv.P_Summary", R.layout.advisor_info));
-	}
-
-	private static void addPage(Page page) {
-		items.add(page);
-		item_map.put(page.url, page);
-	}
-
 	public static void loadResources() {
-		for (Page i : items) {
+		for (Page i : Page.items) {
 			loadResource(i);
 		}
-
 	}
 
 	public static void loadResource(Page page) {
--- src/edu/wpi/tmathmeyer/mybannerwebwpi/page/Page.java
+++ src/edu/wpi/tmathmeyer/mybannerwebwpi/page/Page.java
@@ -1,14 +1,31 @@
 package edu.wpi.tmathmeyer.mybannerwebwpi.page;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import edu.wpi.tmathmeyer.mybannerwebwpi.R;
 
 public abstract class Page {
 
-	public static final Page[] PAGES = {new MailBox("Mail Box Information", "hwwkboxs.P_ViewBoxs", R.layout.mailbox),
-	new MealPlan("Meal Plan Balances", "hwwkcbrd.P_Display", R.layout.meal_plan),
-	new CalendarSchedule("Calendar Schedule", "bwskfshd.P_CrseSchd?start_date_in=[DATE]", R.layout.calendar_schedule),
-	new DetailSchedule("Detail Schedule", "bwskfshd.P_CrseSchdDetl", R.layout.detail_schedule),
-	new AdvisorInfo("Acedemic Advisor Information", "hwwksadv.P_Summary", R.layout.advisor_info)};
+
+	public static List<Page> items = new ArrayList<Page>();
+	public static Map<String,Page> item_map = new HashMap<String,Page>();
+	
+	static {
+		addPage(new MailBox("Mail Box Information", "hwwkboxs.P_ViewBoxs", R.layout.mailbox));
+		addPage(new MealPlan("Meal Plan Balances", "hwwkcbrd.P_Display", R.layout.meal_plan));
+		addPage(new CalendarSchedule("Calendar Schedule", "bwskfshd.P_CrseSchd?start_date_in=[DATE]", R.layout.calendar_schedule));
+		addPage(new DetailSchedule("Detail Schedule", "bwskfshd.P_CrseSchdDetl", R.layout.detail_schedule));
+		addPage(new AdvisorInfo("Acedemic Advisor Information", "hwwksadv.P_Summary", R.layout.advisor_info));
+	}
+	
+	private static void addPage(Page page) {
+		items.add(page);
+		item_map.put(page.url, page);
+	}
+
 	
 	public String url;
 	public String title;
